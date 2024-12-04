f966cc7c48f681fd1689c41c7af633f1aa051633
Tino <tinotendachings@gmail.com>
2024-12-04 12:36:36 +0200

    Initialising repository

---
+++ backend/src/test/java/tinotendachingwena/website/TinotendaChingwenaWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package tinotendachingwena.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinotendaChingwenaWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
