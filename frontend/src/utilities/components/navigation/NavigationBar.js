edfe7e28e66d60dea93ee22478041cdaa0c8603d
Tino <tinotendachings@gmail.com>
2024-12-11 11:23:38 +0200

    refactor: Added improved formatting for the dropdown items for the navigation bar

--- frontend/src/utilities/components/navigation/NavigationBar.js
+++ frontend/src/utilities/components/navigation/NavigationBar.js
@@ -32,18 +32,22 @@ export default function NavigationBar() {
                                     Socials
                                 </a>
                                 <div className="dropdown-menu" aria-labelledby="socialsMenuLink">
-                                    <a className="dropdown-item a" title="Tinotenda Chingwena's Facebook page" aria-label="Opens Tinotenda Chingwena's facebook page"
+                                    <a className="dropdown-item" title="Tinotenda Chingwena's Facebook page" aria-label="Opens Tinotenda Chingwena's facebook page"
                                         href="https://www.facebook.com/tchingwena1" target="_blank">
-                                        <img className="linkimg" src="socialMedia/facebook.svg" alt="Facebook Logo" /> <span>Facebook</span></a>
-                                    <a className="dropdown-item a" title="Tinotenda Chingwena's LinkedIn page" aria-label="Opens Tinotenda Chingwena's LinkedIn page"
+                                        <img className="linkimg" src="socialMedia/facebook.svg" alt="Facebook Logo" />
+                                        <span>Facebook</span>
+                                    </a>
+                                    <a className="dropdown-item" title="Tinotenda Chingwena's LinkedIn page" aria-label="Opens Tinotenda Chingwena's LinkedIn page"
                                         href="http://www.linkedin.com/in/tinotenda-chingwena-6034a6211"
                                         target="_blank">
-                                            <img src="socialMedia/linkedin.svg" alt="Linkedin Logo" className="linkimg" />
-                                        <span>LinkedIn</span></a>
-                                    <a className="dropdown-item a" title="Tinotenda Chingwena's Github page" aria-label="Opens Tinotenda Chingwena's GitHub page"
+                                        <img src="socialMedia/linkedin.svg" alt="Linkedin Logo" className="linkimg" />
+                                        <span>LinkedIn</span>
+                                    </a>
+                                    <a className="dropdown-item" title="Tinotenda Chingwena's Github page" aria-label="Opens Tinotenda Chingwena's GitHub page"
                                         href="https://github.com/tinochings" target="_blank">
-                                        <img src="socialMedia/githublogo.svg" alt="Github Logo" className="linkimg" /> 
-                                        <span>Github</span> </a>
+                                        <img src="socialMedia/githublogo.svg" alt="Github Logo" className="linkimg" />
+                                        <span>Github</span>
+                                    </a>
                                 </div>
                             </li>
                         </ul>
