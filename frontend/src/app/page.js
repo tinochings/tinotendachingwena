259688e015830cf09be67686ac1ad1cf638aa748
Tino <tinotendachings@gmail.com>
2024-12-14 18:05:34 +0200

    refactor: Added remaining CV items

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -3,7 +3,7 @@ import { Row, Col } from "react-bootstrap";
 import homePageStyle from './homepage.module.css';
 import MetaData from "@/utilities/components/MetaData";
 import { CurriculumnVitaeItem, LogoContainer } from "@/utilities/components/homepage/homepageComponents";
-import { deltaBeveragesCvItem } from "@/utilities/services/homepage/hompageServices";
+import { deltaBeveragesCvItem, poetsKingdomCvItem, tiisGlamStudioCvItem, trinMediaCvItem, zvevatsungaSteelAndPlumbingCvItem } from "@/utilities/services/homepage/hompageServices";
 
 export default function Home() {
   return (
@@ -32,8 +32,14 @@ function IntroductionSection() {
                 Frontend and backend. To whichever end, I am a supreme artisan. Eloquent, efficient and maintainable code
                 are embedded in my dna. My true passion lies in programming. I have a solid foundation through my Bachelors Of Science
                 in <b>Computer Science</b> from <b>VU Amsterdam</b>.
-                This foundation can be shown in the languages and frameworks I have mastered as follows:
+                This foundation can be shown in the languages and frameworks I have mastered. Furthermore the current website you are viewing
+                was made using a React framework Next.js for the frontend. The backen was made using Spring Boot and the source code can be
+                found at <a id={`${homePageStyle.personalLink}`} href="https://github.com/tinochings/tinotendachingwena" target='_blank'>Tinotenda Chingwena Source code</a>.
+                Hey! Look below it's me :)
               </p>
+            </div>
+            <div id={`${homePageStyle.personalImageCont}`}>
+            <img id={`${homePageStyle.personalimage}`} src="/socialMedia/personalphotodupe.png"></img>
             </div>
           </main>
         </Col>
@@ -47,10 +53,11 @@ function IntroductionSection() {
               <LogoContainer classList={`${homePageStyle.logoParent}`} imageUrl={"/logos/html.png"} imageAlt={"Html Logo"} title={"HTML"} />
               <LogoContainer classList={`${homePageStyle.logoParent}`} imageUrl={"/logos/css.png"} imageAlt={"Css Logo"} title={"CSS"} />
               <LogoContainer classList={`${homePageStyle.logoParent}`} imageUrl={"/logos/javascript.png"} imageAlt={"Javascript Logo"} title={"JavaScript"} />
-              <LogoContainer classList={`${homePageStyle.logoParent}`} imageUrl={"/logos/react.svg"} imageAlt={"React Logo"} title={"React"} />
+              <LogoContainer classList={`${homePageStyle.logoParent}`} imageUrl={"/logos/react.png"} imageAlt={"React Logo"} title={"React"} />
               <LogoContainer classList={`${homePageStyle.logoParent}`} imageUrl={"/logos/next.svg"} imageAlt={"Nextjs Logo"} title={"Nextjs"} />
               <LogoContainer classList={`${homePageStyle.logoParent}`} imageUrl={"/logos/spring.svg"} imageAlt={"Spring Logo"} title={"Spring"} />
               <LogoContainer classList={`${homePageStyle.logoParent}`} imageUrl={"/logos/postgresql.png"} imageAlt={"Postgresql Logo"} title={"PostgresSQL"} />
+              <LogoContainer classList={`${homePageStyle.logoParent}`} imageUrl={"/logos/bootstrap.png"} imageAlt={"Bootstrap Logo"} title={"Bootstrap"} />
             </div>
           </div>
         </Col>
@@ -58,8 +65,8 @@ function IntroductionSection() {
           <span className="h1">Perfecting the art of Software Engineering:</span>
           <div className={`${homePageStyle.engineeringLogos}`}>
             <LogoContainer classList={`${homePageStyle.logoParent}`} imageUrl={"/logos/github.svg"} imageAlt={"Github logo"} title={"Github"} />
-            <LogoContainer classList={`${homePageStyle.logoParent}`} imageUrl={"/logos/jira.svg"} imageAlt={"Jira logo"} title={"Jira"} />
-            <LogoContainer classList={`${homePageStyle.logoParent}`} imageUrl={"/logos/uml.svg"} imageAlt={"Unified Modelling Language logo"} title={"Unified Modelling Language"} />
+            <LogoContainer classList={`${homePageStyle.logoParent}`} imageUrl={"/logos/jira.png"} imageAlt={"Jira logo"} title={"Jira"} />
+            <LogoContainer classList={`${homePageStyle.logoParent}`} imageUrl={"/logos/uml.png"} imageAlt={"Unified Modelling Language logo"} title={"Unified Modelling Language"} />
             <LogoContainer classList={`${homePageStyle.logoParent}`} imageUrl={"/logos/kiss.png"} imageAlt={"Keep it simple stupid logo"} title={"Keep it simple stupid"} />
           </div>
         </div>
@@ -69,7 +76,7 @@ function IntroductionSection() {
 }
 /**
  * 
- * @returns 
+ * @returns JSX for Curriculumn Vitae Section
  */
 function CurriculumnVitae() {
   return (
@@ -79,7 +86,10 @@ function CurriculumnVitae() {
           <h3 style={{ fontWeight: 900 }}>Curriculumn Vitae</h3>
           <ul id={`${homePageStyle.cvListCont}`}>
             <CurriculumnVitaeItem cvItem={deltaBeveragesCvItem()}></CurriculumnVitaeItem>
-            <CurriculumnVitaeItem cvItem={deltaBeveragesCvItem()}></CurriculumnVitaeItem>
+            <CurriculumnVitaeItem cvItem={tiisGlamStudioCvItem()}></CurriculumnVitaeItem>
+            <CurriculumnVitaeItem cvItem={zvevatsungaSteelAndPlumbingCvItem()}></CurriculumnVitaeItem>
+            <CurriculumnVitaeItem cvItem={trinMediaCvItem()}></CurriculumnVitaeItem>
+            <CurriculumnVitaeItem cvItem={poetsKingdomCvItem()}></CurriculumnVitaeItem>
           </ul>
 
         </Col>
--- frontend/src/utilities/services/homepage/hompageServices.js
+++ frontend/src/utilities/services/homepage/hompageServices.js
@@ -1,4 +1,4 @@
-import Cvitem from "@/utilities/models/cvitem";
+import Cvitem, { LinkItem } from "@/utilities/models/cvitem";
 
 /**
  * 
@@ -6,7 +6,7 @@ import Cvitem from "@/utilities/models/cvitem";
  */
 export function deltaBeveragesCvItem() {
     let deltaBeverages = new Cvitem("08/2024 - Current", "Delta Beverages - Transport Services", "I.C.T Specialist",
-         [], ['Resource Management', 'Systems Integration', 'SAP']);
+        [], ['SAP', 'Resource Management', 'Systems Integration']);
 
     deltaBeverages.description = `Leverage my expertise in software development to provide acute insight 
     in integrating new software systems into Delta's technological ecosystem. Provide technical software support for applications such as
@@ -14,4 +14,70 @@ export function deltaBeveragesCvItem() {
     of computer literacy within the organization by the provision of ICT training and workshops `;
 
     return deltaBeverages;
+}
+
+/**
+ * 
+ * @returns instantiated Tiis Glam Studio CV ITEM 
+ */
+export function tiisGlamStudioCvItem() {
+    let tiisGlamStudio = new Cvitem("01/2024 - 07/2024", "Tiis Glam Studio", "Software Engineer - Frontend & Backend",
+        [], ['UML', 'Java', 'Thymeleaf', 'HTML & CSS', 'JavaScript', 'Spring Boot', 'Jetpack Compose', 'Kotlin & Android']);
+
+    tiisGlamStudio.description = `Designed algorithms, system design, system protocols with UML and utilised Jira for project management and maintenance.
+    Constructed website front-end user 
+    interface using CSS, HTML5, JavaScript, Thymleaf and Bootstrap. Constructed Android application primarily with Kotlin. Implemented Android 
+    user-interface using 100% Jetpack Compose with a MVVM (Model-View-View-Model). Implement Spring Boot for backend with PostgreSQL database,
+    thread safety and cryptography for communication with android client. Implemented hybrid encryption. RSA was used for end-to-end encryption.
+     AES Galois/Counter mode was used for symmetric encryption.`;
+
+    return tiisGlamStudio;
+}
+
+/**
+ * 
+ * @returns instantiated Tiis Glam Studio CV ITEM 
+ */
+export function zvevatsungaSteelAndPlumbingCvItem() {
+    let zvevatsunga = new Cvitem("08/2023 - 03/2024", "Zvevatsunga Steel & Plumbing", "WEB DEVELOPER - FRONT-END & BACK END",
+        [], ['Java', 'Bootstrap', 'Thymeleaf', 'HTML & CSS', 'JavaScript', 'Spring Boot']);
+
+    zvevatsunga.description = `Construct front-end user interface using CSS, HTML5, JavaScript, and Bootstrap.  Implement Spring Boot for 
+    backend with PostgreSQL database. Execute thorough testing procedures, including unit tests and system tests, to identify
+    and rectify potential issues and ensure system's reliability. Implement caching mechanisms to optimize data retrieval, reducing latency and
+    enhancing overall server response times`;
+
+    return zvevatsunga;
+}
+
+/**
+ * 
+ * @returns instantiated Trin Media CV ITEM 
+ */
+export function trinMediaCvItem() {
+    let trinMedia = new Cvitem("05/2023 - 07/2023", "Trin Media Consultancy Agency", "SOFTWARE PROGRAMMER",
+        [], ['Java', 'Bootstrap', 'Thymeleaf', 'HTML & CSS', 'JavaScript', 'Spring Boot']);
+
+    trinMedia.description = `Construct front-end user interface using CSS, HTML5, JavaScript, and Bootstrap.  Implement Spring Boot for 
+    backend with PostgreSQL database. Implemented coding practices to guarantee cross-browser compatibility, optimizingthe user experience 
+    across various web platforms. Implemented JavaScript to create interactive features, fostering user engagement and providing a dynamic 
+    browsing experience. Conducted rigorous code reviews and adhered to best practices, maintaining high standards of code quality and readability`;
+
+    return trinMedia;
+}
+
+/**
+ * 
+ * @returns instantiated Poets Kingdom CV ITEM 
+ */
+export function poetsKingdomCvItem() {
+    let poetsKingdomLinkItem = new LinkItem('https://github.com/tinochings/PoetsKingdom', 'Poets Kingdom Source Code', '/miscellaneous/link.svg')
+    let poetsKingdom = new Cvitem("05/2023 - Ongoing", "Open Source Project", "Android Developer",
+        [poetsKingdomLinkItem], ['XML','MVVM', 'Kotlin', 'Android', 'Jetpack Compose']);
+
+    poetsKingdom.description = `Hey! why not check it out yourself? ;) Implemented user interface using Jetpack Compose (Approx 90%) and XML (Approx 10%). 
+    Incorporates multi-threading using Kotlin coroutines to prevent main thread blocking, run long intensive tasks and ensure a seamless user experience.
+    Incorporates Model-View-View-Model (MVVM) design pattern to ensure testability in isolation and ensures a separation of business and user interface logic.`;
+
+    return poetsKingdom;
 }
