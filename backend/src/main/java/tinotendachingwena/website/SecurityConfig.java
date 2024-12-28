514a87075ef1b2a23b3223c4dd6fd6f63c132fb2
Tino <tinotendachings@gmail.com>
2024-12-28 22:39:58 +0200

    configuration: Removed in memory authentication provided by Spring Security. Furthermore, added configuration for a cookie based CSRF and stateless sessions

--- backend/src/main/java/tinotendachingwena/website/SecurityConfig.java
+++ backend/src/main/java/tinotendachingwena/website/SecurityConfig.java
@@ -1,4 +1,27 @@
 package tinotendachingwena.website;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((auth) -> auth.anyRequest().permitAll())
+                .csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .sessionManagement(httpSecuritySessionManagementConfigurer ->
+                        httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .requiresChannel(channelRequestMatcherRegistry ->
+                        channelRequestMatcherRegistry.requestMatchers(request -> request.getHeader("X-Forwarded-Proto") != null)
+                                .requiresSecure());
+
+        return http.build();
+    }
 }
