bd9732bb432c04a5a382fddbf31cc9487aca2d06
Tino <tinotendachings@gmail.com>
2025-01-02 15:12:06 +0200

    feature: Added experience api endpoint and supporting service for information retrieval

--- backend/src/main/java/tinotendachingwena/website/controllers/api/experiences/ExperiencesAPI.java
+++ backend/src/main/java/tinotendachingwena/website/controllers/api/experiences/ExperiencesAPI.java
@@ -1,9 +1,32 @@
 package tinotendachingwena.website.controllers.api.experiences;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import tinotendachingwena.website.models.experiences.ProjectItem;
+import tinotendachingwena.website.services.experiences.ExperienceAPIService;
 
-@RestController
+@Controller
 @RequestMapping("/api/experiences")
 public class ExperiencesAPI {
+
+    private final ExperienceAPIService experienceAPIService;
+
+    public ExperiencesAPI(ExperienceAPIService experienceAPIService) {
+        this.experienceAPIService = experienceAPIService;
+    }
+
+    @GetMapping(value = "/{language}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ProjectItem[]> projects(@PathVariable String language){
+        ProjectItem[] projectItems = experienceAPIService.getExperiencesEn(language);
+        if (projectItems != null)
+            return new ResponseEntity<>(projectItems, HttpStatus.OK);
+
+        throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
