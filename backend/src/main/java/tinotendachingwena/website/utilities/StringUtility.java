0028894208cbc59e43d7f8d6e13e5cc8ea97ceb4
Tino <tinotendachings@gmail.com>
2025-01-05 20:58:31 +0200

    resource: Added a global static string utility class

---
+++ backend/src/main/java/tinotendachingwena/website/utilities/StringUtility.java
@@ -0,0 +1,6 @@
+package tinotendachingwena.website.utilities;
+
+public class StringUtility {
+    public static String invalidMethodResponseHeader = "Invalid Input";
+    public static String contactFormSuccessMessage = "Message sent successfully :)";
+}
