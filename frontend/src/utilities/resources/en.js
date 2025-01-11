4094e7ef4156b2a982482246bcbebb3d2be8d3ec
Tino <tinotendachings@gmail.com>
2025-01-11 13:42:44 +0200

    resource: Rectifying spelling errors

--- frontend/src/utilities/resources/en.js
+++ frontend/src/utilities/resources/en.js
@@ -14,7 +14,7 @@ export const metaDataResourceExperiences = {
 }
 export const enresource = {
     statement1: `Frontend and backend. To whichever end, I am a supreme artisan. Eloquent, efficient and maintainable code
-                are embedded in my dna. My true passion lies in programming. I have a solid foundation through my Bachelors Of Science
+                are embedded in my DNA. My true passion lies in programming. I have a solid foundation through my Bachelors Of Science
                 in`,
     statement2: `This foundation can be shown in the languages and frameworks I have mastered. Furthermore the current website you are viewing
                 was made using a React framework Next.js for the frontend. The backend was made using Spring Boot and the source code can be
