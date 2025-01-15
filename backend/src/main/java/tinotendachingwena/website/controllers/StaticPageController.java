c32f870dfbf95558de926d7591402e0012331ba2
Tino <tinotendachings@gmail.com>
2025-01-15 13:11:18 +0200

    Testing if API is reached

--- backend/src/main/java/tinotendachingwena/website/controllers/StaticPageController.java
+++ backend/src/main/java/tinotendachingwena/website/controllers/StaticPageController.java
@@ -1,5 +1,6 @@
 package tinotendachingwena.website.controllers;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -8,6 +9,8 @@ public class StaticPageController {
 
     @GetMapping("/")
     public String home(){
+        System.out.println("I am HERERRER");
+        System.out.println((new ClassPathResource("en.html")).exists());
         return "/en.html";
     }
 }
